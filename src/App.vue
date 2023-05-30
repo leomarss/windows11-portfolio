f669b0a42122baac940c14b5ba8d0cecdb03377f
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-05-30 17:49:36 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+// import MyComponent from "./components/MyComponent.vue";
+
+export default {
+  data() {
+    return {
+      title: "Hello world",
+    };
+  },
+
+  // components: {
+  //   MyComponent,
+  // },
+};
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style lang="scss" scoped></style>
