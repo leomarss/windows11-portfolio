a05d5df820fc9a77ae3bc14242ba12581175883c
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-05-30 18:15:31 +0200

    Central icons

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,29 @@ export default {
 
 <template>
   <div class="desktop"></div>
-  <div class="menu-bar">content</div>
+  <div class="menu-bar flex">
+    <!-- Icons -->
+    <div class="central-icons h-full flex">
+      <button class="selected-item">
+        <img class="max-w-full max-h-full" src="/images/menu-bar/windows.png" />
+      </button>
+      <button class="selected-item">
+        <img class="max-w-full max-h-full" src="/images/menu-bar/explorer.png" />
+      </button>
+      <button class="selected-item">
+        <img class="max-w-full max-h-full" src="/images/menu-bar/vscode.png" />
+      </button>
+      <button class="selected-item">
+        <img class="max-w-full max-h-full" src="/images/menu-bar/terminal.png" />
+      </button>
+    </div>
+
+    <div class="left-menu-bar">
+      <button class="selected-item lang">
+        <span>ENG</span>
+      </button>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
@@ -30,5 +52,17 @@ $menu-bar-h: 50px;
   height: $menu-bar-h;
   border-top: 1px solid #3f3f3f;
   background-color: #212121;
+  padding: 5px;
+
+  .central-icons {
+    & > button {
+      padding: 8px;
+      border-radius: 5px;
+    }
+  }
+}
+
+.selected-item:hover {
+  background-color: #2e2e2e;
 }
 </style>
